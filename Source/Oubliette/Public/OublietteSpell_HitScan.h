80d83e439a9787b834f41d1dc3d1854b15522283
ConnorRowe <connormagson@hotmail.co.uk>
2019-07-01 21:32:09 +0100

    Added hit-scan spell type.
    Currently very basic implementation where it deals damage in a small AOE.

---
+++ Source/Oubliette/Public/OublietteSpell_HitScan.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OublietteSpell.h"
+#include "OublietteSpell_HitScan.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OUBLIETTE_API AOublietteSpell_HitScan : public AOublietteSpell
+{
+	GENERATED_BODY()
+	
+};
