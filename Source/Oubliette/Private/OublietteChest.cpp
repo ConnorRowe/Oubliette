28be4c5a05251646a1f38a7b3255fa0b90df1f37
ConnorRowe <connormagson@hotmail.co.uk>
2019-07-11 02:22:04 +0100

    Added a Chest
    Proper spawning logic is NYI so for now they can be spawned with ctrl-F for testing purposes.

---
+++ Source/Oubliette/Private/OublietteChest.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OublietteChest.h"
+
+// Sets default values
+AOublietteChest::AOublietteChest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOublietteChest::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOublietteChest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
